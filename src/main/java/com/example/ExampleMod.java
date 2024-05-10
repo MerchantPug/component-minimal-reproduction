1699d97cdda97399a8b7b98ad7ea3a34e842d63b
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-10 18:29:08 +1000

    Minimization.

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -5,23 +5,21 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.DataComponentType;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
-
-import java.util.Map;
 
 public class ExampleMod implements ModInitializer {
 	public static final RegistryKey<Registry<ExampleRecord>> RECORD_REGISTRY_KEY = RegistryKey.ofRegistry(new Identifier("example", "record"));
 	public static final RegistryKey<ExampleRecord> BOOGLY = RegistryKey.of(RECORD_REGISTRY_KEY, new Identifier("example", "boogly"));
-	public static final DataComponentType<ExampleComponent> RECORD_COMPONENT = DataComponentType.<ExampleComponent>builder()
-			.codec(ExampleComponent.CODEC)
-			.packetCodec(ExampleComponent.PACKET_CODEC)
+	public static final DataComponentType<RegistryEntry<ExampleRecord>> RECORD_COMPONENT = DataComponentType.<RegistryEntry<ExampleRecord>>builder()
+			.codec(ExampleRecord.CODEC)
+			.packetCodec(ExampleRecord.PACKET_CODEC)
 			.build();
 	public static final Item ITEM = Registry.register(Registries.ITEM, new Identifier("example", "item"), new Item(new Item.Settings().maxCount(1)));
 
@@ -29,9 +27,9 @@ public class ExampleMod implements ModInitializer {
 	public void onInitialize() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
 			ItemStack stack = new ItemStack(ITEM);
-			stack.set(RECORD_COMPONENT, new ExampleComponent(Map.of(entries.getContext().lookup().createRegistryLookup().getOrThrow(RECORD_REGISTRY_KEY).getOrThrow(BOOGLY), Enchantments.AQUA_AFFINITY.getRegistryEntry())));
+			stack.set(RECORD_COMPONENT, entries.getContext().lookup().createRegistryLookup().getOrThrow(RECORD_REGISTRY_KEY).getOrThrow(BOOGLY));
 			entries.add(stack);
 		});
-		DynamicRegistries.registerSynced(RECORD_REGISTRY_KEY, ExampleRecord.CODEC);
+		DynamicRegistries.registerSynced(RECORD_REGISTRY_KEY, ExampleRecord.DIRECT_CODEC);
 	}
 }
--- src/main/java/com/example/ExampleRecord.java
+++ src/main/java/com/example/ExampleRecord.java
@@ -2,9 +2,17 @@ package com.example;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryFixedCodec;
 
 public record ExampleRecord(int value) {
-    public static final Codec<ExampleRecord> CODEC = RecordCodecBuilder.create(inst -> inst.group(
+    public static final Codec<ExampleRecord> DIRECT_CODEC = RecordCodecBuilder.create(inst -> inst.group(
             Codec.INT.fieldOf("value").forGetter(ExampleRecord::value)
     ).apply(inst, ExampleRecord::new));
+    public static final Codec<RegistryEntry<ExampleRecord>> CODEC = RegistryFixedCodec.of(ExampleMod.RECORD_REGISTRY_KEY);
+
+    public static final PacketCodec<RegistryByteBuf, RegistryEntry<ExampleRecord>> PACKET_CODEC = PacketCodecs.registryEntry(ExampleMod.RECORD_REGISTRY_KEY);
 }
